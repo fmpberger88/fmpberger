2e653306ef9c7b02ff96844065e40ee52360df2f
fmpberger88 <fabian.berger@protonmail.ch>
2024-03-12 22:53:31 +0100

    initialize project

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Header () {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Education</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
