e8ec113c8d8e026ece08b4e8eeef863bdc9a8baa
fmpberger88 <fabian.berger@protonmail.ch>
2024-03-12 23:04:37 +0100

    simple styling to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Header from "./components/header.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-
+import styles from './Header.module.css'
 
 export default function Header () {
     return (
         <header>
             <nav>
                 <ul>
-                    <li>Home</li>
                     <li>About</li>
                     <li>Education</li>
+                    <li>Experience</li>
                     <li>Contact</li>
                 </ul>
             </nav>
