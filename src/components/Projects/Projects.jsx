f818def9330e6190eb9542b008deb0d1d30be076
fmpberger88 <fabian.berger@protonmail.ch>
2024-03-14 22:52:23 +0100

    adding grid layout and images

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,40 @@
 import styles from './Projects.module.css';
+import el from '../../assets/images/el-info - Ergänzungsleistungen.webp';
+import nikita from '../../assets/images/nikita.webp';
+import trix from '../../assets/images/Trix Berger - Zentralschweizerische Kunstschaffende in Malerei, Keramik, Stoffe.webp';
+import camera from '../../assets/images/format_camera.webp';
 
 export default function Projects() {
     return (
         <div className={styles.gridContainer}>
-            <div>Project-1</div>
-            <div>Project-2</div>
-            <div>Project-3</div>
-            <div>Project-4</div>
+            <div className={styles.firstProject}>
+                <img className={styles.img} src={el}/>
+            </div>
+            <div className={styles.firstDescription}>
+                <a href="#">Source Code</a>
+                <a href="#">Live Page</a>
+            </div>
+            <div className={styles.secondDescription}>
+                <a href="#">Source Code</a>
+                <a href="#">Live Page</a>
+            </div>
+            <div className={styles.secondProject}>
+                <img className={styles.img} src={nikita}/>
+            </div>
+            <div className={styles.thirdDescription}>
+                <a href="#">Source Code</a>
+                <a href="#">Live Page</a>
+            </div>
+            <div className={styles.thirdProject}>
+                <img className={styles.img} src={trix}/>
+            </div>
+            <div className={styles.fourthDescription}>
+                <a href="#">Source Code</a>
+                <a href="#">Live Page</a>
+            </div>
+            <div className={styles.fourthProject}>
+                <img className={styles.img} src={camera}/>
+            </div>
         </div>
-    )
+    );
 }
