2b77bd787ad3487efdaddebd133ad41441b67c30
fmpberger88 <fabian.berger@protonmail.ch>
2024-03-20 23:33:21 +0100

    adding AOS and responsive design for smartphones

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,11 +3,21 @@ import el from '../../assets/images/el-info - Ergänzungsleistungen.webp';
 import nikita from '../../assets/images/nikita.webp';
 import trix from '../../assets/images/Trix Berger - Zentralschweizerische Kunstschaffende in Malerei, Keramik, Stoffe.webp';
 import camera from '../../assets/images/format_camera.webp';
+import {useEffect} from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Projects() {
+    useEffect(() => {
+        AOS.init({
+            // hier kannst du globale Einstellungen für AOS setzen
+            duration : 3000 // Dauer der Animationen in Millisekunden
+        });
+    }, []);
+
     return (
         <div className={styles.gridContainer}>
-            <div className={styles.firstProject}>
+            <div className={styles.firstProject} data-aos="fade-right">
                 <img className={styles.img} src={el}/>
             </div>
             <div className={styles.firstDescription}>
@@ -18,21 +28,21 @@ export default function Projects() {
                 <a href="#">Source Code</a>
                 <a href="#">Live Page</a>
             </div>
-            <div className={styles.secondProject}>
+            <div className={styles.secondProject} data-aos="fade-left">
                 <img className={styles.img} src={nikita}/>
             </div>
             <div className={styles.thirdDescription}>
                 <a href="#">Source Code</a>
                 <a href="#">Live Page</a>
             </div>
-            <div className={styles.thirdProject}>
+            <div className={styles.thirdProject} data-aos="fade-right">
                 <img className={styles.img} src={trix}/>
             </div>
             <div className={styles.fourthDescription}>
                 <a href="#">Source Code</a>
                 <a href="#">Live Page</a>
             </div>
-            <div className={styles.fourthProject}>
+            <div className={styles.fourthProject} data-aos="fade-left">
                 <img className={styles.img} src={camera}/>
             </div>
         </div>
