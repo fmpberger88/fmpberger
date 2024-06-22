11ae3bb802a8344bbec2b8b25430b74e55b92ca5
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-22 23:02:50 +0200

    implement api

---
+++ src/api.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5001/api/v1';
+
+export const fetchBlogs = async () => {
+    const { data } = await axios.get(`${API_URL}/blogs`);
+    return data;
+}
+
+export const fetchBlogById = async (id) => {
+    const { data } = await axios.get(`${API_URL}/blogs/${id}`);
+    return data;
+}
+
+export const fetchCommentsByBlogId = async (blogId) => {
+    const { data } = await axios.get(`${API_URL}/comments/${blogId}`);
+    return data;
+}
+
