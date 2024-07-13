c1c631558841048968288cc83cb3c78060e3bd13
fmpberger88 <fabian.berger@protonmail.ch>
2024-07-13 17:37:41 +0200

    refactor tools - set cardFront and cardBack

--- src/components/Tools/Tools.jsx
+++ src/components/Tools/Tools.jsx
@@ -8,28 +8,30 @@ import r from '../../assets/icons/r.svg'
 
 
 export default function Tools() {
+
+    const languages = [
+        { id: 1, icon: html, label: 'HTML' , years: "3"},
+        { id: 2, icon: css, label: 'CSS' , years: "3"},
+        { id: 3, icon: js, label: 'Javascript' , years: "3"},
+        { id: 4, icon: python, label: 'Python' , years: "4"},
+        { id: 5, icon: r, label: 'R' , years: "5"},
+    ]
+
     return (
         <section>
             <h2 className={styles.sectionTitle} id="tools">Tools</h2>
             <div className={styles.language}>
-                <div className={styles.box}>
-                    <img src={html} alt="HTML" width="50" height="50"/>
-                </div>
-                <div className={styles.box}>
-                    <img src={css} alt="CSS" width="50" height="50"/>
-                </div>
-                <div className={styles.box}>
-                    <img src={js} alt="Javascript" width="50" height="50"/>
-                </div>
-                <div className={styles.box}>
-                    <img src={python} alt="Python" width="50" height="50"/>
-                </div>
-                <div className={styles.box}>
-                    <img src={ruby} alt="Ruby" width="50" height="50"/>
-                </div>
-                <div className={styles.box}>
-                    <img src={r} alt={"R"} width="50" height="50"/>
-                </div>
+                { languages.map(language => (
+                    <div key={language.id} className={styles.card}>
+                        <div className={styles.cardFront}>
+                            <img src={language.icon} alt={language.label} width="150" height="150" />
+                        </div>
+                        <div className={styles.cardBack}>
+                            <span className={styles.cardLabel}><strong>{language.label}</strong></span>
+                            <span>{language.years} years of experience</span>
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
     )
